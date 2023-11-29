2b5e653f6123389f46dea56c6fba0d03cc5d8882
Ray <arthur.shifengrui@gmail.com>
2023-11-29 16:08:29 +0000

    elastic search

--- chainklik/abi/views.py
+++ chainklik/abi/views.py
@@ -1,11 +1,26 @@
 from django.http import HttpResponse
+from elasticsearch import Elasticsearch
 import json
+
+es = Elasticsearch(
+    "https://localhost:9200",
+    http_auth=('elastic', 'y=fUp=8ucKL18I5K=1Am'),
+    verify_certs=False)
 
 def index(request):
     return HttpResponse("Hello, world. You're at the abi index.")
   
 def contract_abi(request, contract_address):
-    data = json.dumps({"contract":contract_address})
+    index_name = 'abi'
+    body={"size" : 100, "query":{ "match":{ "address": contract_address }, "match":{ "type": "contract" } } }
+    resp = es.search(index=index_name, body=body)
+    print(len(resp['hits']['hits']))
+    if len(resp['hits']['hits']) > 0:
+        result = resp['hits']['hits'][0]['_source']
+        data = json.dumps([result['_source'] for result in resp['hits']['hits']])
+        return HttpResponse(data, content_type='application/json')
+    
+    data = json.dumps({})
     return HttpResponse(data, content_type='application/json')
   
 def function_abi(request, contract_address, function_name):
