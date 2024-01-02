e16095dcde4fd1f2cc55646d5a236cbd19da3e3d
Ray <arthur.shifengrui@gmail.com>
2024-01-02 17:01:16 +0000

    node test

--- chainklik/config/config.py
+++ chainklik/config/config.py
@@ -2,5 +2,5 @@ config = {
     "eth_mainnet_http_url":"https://stylish-wider-patina.quiknode.pro/274049e810f4dee5f0807cd426196aeafd438838/",
     "eth_mainnet_ws_url":"wss://stylish-wider-patina.quiknode.pro/274049e810f4dee5f0807cd426196aeafd438838/",
     "eth_sepolia_http_url":"https://dark-fittest-wish.ethereum-sepolia.quiknode.pro/316a3afe247843045deee44ffd179aa6761254c5/",
-    "eth_mainnet_ws_url":"wss://dark-fittest-wish.ethereum-sepolia.quiknode.pro/316a3afe247843045deee44ffd179aa6761254c5/"
+    "eth_sepolia_ws_url":"wss://dark-fittest-wish.ethereum-sepolia.quiknode.pro/316a3afe247843045deee44ffd179aa6761254c5/"
 }
