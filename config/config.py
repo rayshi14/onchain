294522ab4959653d6296f4998c52256ef8bf077d
Ray <arthur.shifengrui@gmail.com>
2024-01-02 14:25:48 +0000

    node test

--- config/config.py
+++
@@ -1,4 +0,0 @@
-config = {
-    "http_url":"https://stylish-wider-patina.quiknode.pro/274049e810f4dee5f0807cd426196aeafd438838/",
-    "ws_url":"wss://stylish-wider-patina.quiknode.pro/274049e810f4dee5f0807cd426196aeafd438838/"
-}
